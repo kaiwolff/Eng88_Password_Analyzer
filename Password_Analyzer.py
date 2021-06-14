485531c181ad8feb9c001dfc741978d971d6ac7a
unknown <kai.wolff.92@gmail.com>
2021-06-14 14:46:40 +0100

    added to check_list

--- Password_Analyzer.py
+++ Password_Analyzer.py
@@ -1,43 +1,37 @@
 class PasswordChecker:
 
-    def __init__(self):
+    def take_input(self):
+        pass
+        #Take full user name, year of birth. Ask user what option (generate, check, or exit). # For now, just test variables, shoudl be written up by Munira
 
-
-    def take_input:
-        #Take full user name, year of birth. Ask user what option (generate, check, or exit).
-
-        user_firstname = kai#input('Enter your first name:')
-        user_secondname = wolff#input('Enter your first name:')
-        user_birthyear = "1992"#input('Enter your year of birth:')
-        user_password = ""
-
-        # user_input = input("")
-        # if user_input == "end":
-        #     break
-        # if again == no:
-        #     break
-
-    def generate_password:
+    def generate_password(self): # will be written by Afshana
             #check policy, generate random characters that satisfy policy
+        pass
 
-    def check_password(self,password):
+    def check_password(self,password, first_name, second_name, birth_year):
+        print("opened check_password")
         #check password against password policy, their name, DOB, and common passwords.
         report = []
+
         policy_compliant = self.check_policy(password)
+        # print(f"checked policy compliance {policy_compliant}")
 
-        # if policy_compliant:
-        #     report.append("Complies with password policy")
-        # else:
-        #     report.append("Does not comply with password policy")
+        if policy_compliant:
+            report.append("Complies with password policy")
+            # print("Complies with password policy")
+        else:
+            report.append("Does not comply with password policy")
+            # print("Does not comply with password policy")
 
-        # not_common = self.check_list(password)
+        not_common = self.check_list(password)
+        print("common passwords checked")
 
-        # if not_common:
-        #     report.append("Not in list of common passwords")
-        # else:
-        #     report.append("Found in list of common passwords")
+        if not_common:
+            report.append("Not in list of common passwords")
+        else:
+            report.append("Found in list of common passwords")
 
-        user_detail_free = self.check_user_details(password)
+        user_detail_free = self.check_user_details(password, first_name, second_name, birth_year)
         if user_detail_free:
             report.append("No user details in password")
         else:
@@ -50,31 +44,71 @@ class PasswordChecker:
 
     def check_list(self,password):
         #checks if the given password is part of the common password list.
-        password_file = open('common_passwords.txt', 'r')
-
-        while True:
+        with open('common_passwords.txt', 'r') as password_file:
             #iterates through whole file
-            line = password_file.readline()
-            if password in line:
+            for line in password_file:
+                if password in line:
+                    password_file.close()
+                    return False
+                elif line == False:
+                    #line will be empty if nothing left in file
+                    print("end of file reached")
+                    password_file.close()
+                    break
+        return True
+
+
+    def check_policy(self,password):
+            # open policy file, output True or False depending whether password complies
+            policy_list = self.read_password_policy()
+            #now hav ea list defining password policy
+            num_specials = policy_list[0]
+            num_lowercase = policy_list[1]
+            num_uppercase = policy_list[2]
+            num_numbers = policy_list[3]
+            min_length = policy_list[4]
+            max_length = policy_list[5]
+            allowed_specials = policy_list[6]
+
+            count_specials = 0
+            count_lower = 0
+            count_upper = 0
+            count_numbers = 0
+
+            if len(password) < min_length or len(password) > max_length:
+                #not compliant if too short or too long
                 return False
-            elif line == False:
-                #line will be empty if nothing left in file
-                break
-        return True
 
+            for letter in password:
+                #check each letter to see if special, lower, upper, or number. Count each of these
+                if letter.isdigit():
+                    count_numbers += 1
+                elif letter.isupper():
+                    count_upper += 1
+                elif letter.islower():
+                    count_lower += 1
+                elif letter in allowed_specials:
+                    count_specials += 1
+                else:
+                    #return false if part of password is not in any allowed category
+                    return False
 
-def check_policy(self,password):
-        # open policy file, output True or False depending whether password complies
-def check_user_details(self, password, user_firstname, user_lastname, user_birthyear):
-    #checks if the password contains the user name or year of birth. Outputs T
-    if user_firstname in password:
-        return False
-    elif user_lastname in password:
-        return False
-    elif user_birthyear in password:
-        return False
-    else:
-        return True
+            #now have a count of all the lower, upper, special characters and numbers
+            if count_upper >= num_uppercase and count_lower >= num_lowercase and count_specials >= num_specials and count_numbers >= num_numbers:
+                return True
+            else:
+                return False
+
+    def check_user_details(self, password, user_firstname, user_lastname, user_birthyear):
+        #checks if the password contains the user name or year of birth. Outputs T
+        if user_firstname in password:
+            return False
+        elif user_lastname in password:
+            return False
+        elif user_birthyear in password:
+            return False
+        else:
+            return True
 
 
     def generate_report(self,report):
@@ -82,10 +116,9 @@ def check_user_details(self, password, user_firstname, user_lastname, user_birth
         for line in report:
             print(line)
         #ask user if they want to
-        wants_file = input(Would you like this written to a file (y/n) ? )
+        wants_file = input("Would you like this written to a file (y/n) ? ")
         if wants_file.lower() == "y":
             write_to_file(report)
-            return "Report written to file"
         else:
             return "Report not written to file"
 
@@ -93,3 +126,27 @@ def check_user_details(self, password, user_firstname, user_lastname, user_birth
         #writes report to file.
         file = open(password_report.txt, 'w')
         for line in report:
+            write.file(f"{line}\n")
+        return "Report written to password_report.txt"
+
+    def read_password_policy(self):
+
+        # password_policy = open('password_policy.txt', 'r')
+        num_specials = 1
+        num_lowercase = 3
+        num_uppercase = 3
+        num_numbers = 4
+        min_length = 8
+        max_length = 16
+        allowed_specials = "~!\"£$%^&*()_-+="
+
+        return [num_specials, num_lowercase, num_uppercase, num_numbers, min_length, max_length, allowed_specials]
+
+
+
+    #########################TEST
+
+password_tester = PasswordChecker()
+print("password_tester generated")
+password_tester.check_password("test","Kai", "Wolff", "1992")
+
