c48eaedf07c5502fdff84b9ac12d09935ca15a1b
unknown <kai.wolff.92@gmail.com>
2021-06-15 10:15:12 +0100

    configparser for read_policy, file append option added. report now includes tested password.

--- Password_Analyzer.py
+++ Password_Analyzer.py
@@ -1,16 +1,55 @@
+import configparser
+
 class PasswordChecker:
 
     def take_input(self):
-        pass
-        #Take full user name, year of birth. Ask user what option (generate, check, or exit). # For now, just test variables, shoudl be written up by Munira
+        # Take full user name, year of birth. as user what option (generate, check, or exit). Written by MM, AB and KW
+        while True:
+            first_name = input('Enter your first name: ')
+            last_name = input('Enter your last name: ')
+            birth_year = input('Enter your year of birth: ')
+            options_choices = input('Please enter one of the following options: check password(1), generate password(2) or exit(3) ')
 
-    def generate_password(self): # will be written by Afshana
-            #check policy, generate random characters that satisfy policy - Afshana working on this.
-        pass
+            if options_choices == ('1'):
+                password = input("Please input password to check: ")
+                self.check_password(passiword, first_name, last_name, birth_year)
+            elif options_choices == ('2'):
+                self.generate_password(first_name, last_name, birth_year)
+            elif options_choices == ("3"):
+                break
+            else:
+                print("Please choose a valid option ")
+
+            # Should be going back to option_choices
+
+    def generate_password(self, user_firstname, user_lastname, user_birthyear):
+        #Generates a strong password. Written by AB
+
+        policy_checklist = self.read_password_policy()
+        max_length = policy_checklist[5]
+        allowed_characters = list(policy_checklist[6])
+
+        password = ""
+        for character in range(max_length):
+
+            random_num = random.randint(1,4)
+
+            if random_num == 1:
+                password += random.choice(allowed_characters)
+            elif random_num == 2:
+                password += random.choice(string.ascii_lowercase)
+            elif random_num == 3:
+                password += random.choice(string.ascii_uppercase)
+            else:
+                password += random.choice(string.digits)
+
+        print(password)
+
+        # To do: check that the password generated does not take into username or birth year
 
     def check_password(self,password, first_name, second_name, birth_year):
         print("opened check_password")
-        #check password against password policy, their name, DOB, and common passwords.
+        #check password against password policy, their name, DOB, and common passwords. Written by KW
         report = []
 
         policy_compliant = self.check_policy(password)
@@ -44,10 +83,10 @@ class PasswordChecker:
         else:
             report.append("This means that the password is weak")
 
-        return report
+        self.generate_report(report, password)
 
     def check_list(self,password):
-        #checks password against passwords in common_passwords.txt. Returns True if password is not in file, False if found
+        #checks password against passwords in common_passwords.txt. Returns True if password is not in file, False if found.Written by KW
         with open('common_passwords.txt', 'r') as password_file:
             #iterates through whole file
             for line in password_file:
@@ -63,49 +102,50 @@ class PasswordChecker:
 
 
     def check_policy(self,password):
-            # reads password policy, checks if password complies with requirements. Returns True if yes, False if not.
-            policy_list = self.read_password_policy()
-            #now hav ea list defining password policy
-            num_specials = policy_list[0]
-            num_lowercase = policy_list[1]
-            num_uppercase = policy_list[2]
-            num_numbers = policy_list[3]
-            min_length = policy_list[4]
-            max_length = policy_list[5]
-            allowed_specials = policy_list[6]
+        # reads password policy, checks if password complies with requirements. Returns True if yes, False if not. Written by KW
+        policy_list = self.read_password_policy()
+        # print(policy_list)
+        #now hav ea list defining password policy
+        num_specials = policy_list[0]
+        num_lowercase = policy_list[1]
+        num_uppercase = policy_list[2]
+        num_numbers = policy_list[3]
+        min_length = policy_list[4]
+        max_length = policy_list[5]
+        allowed_specials = policy_list[6]
 
-            count_specials = 0
-            count_lower = 0
-            count_upper = 0
-            count_numbers = 0
+        count_specials = 0
+        count_lower = 0
+        count_upper = 0
+        count_numbers = 0
 
-            if len(password) < min_length or len(password) > max_length:
-                #not compliant if too short or too long
-                return False
+        if len(password) < min_length or len(password) > max_length:
+            #not compliant if too short or too long
+            return False
 
-            for letter in password:
-                #check each letter to see if special, lower, upper, or number. Count each of these
-                if letter.isdigit():
-                    count_numbers += 1
-                elif letter.isupper():
-                    count_upper += 1
-                elif letter.islower():
-                    count_lower += 1
-                elif letter in allowed_specials:
-                    count_specials += 1
-                else:
-                    #return false if part of password is not in any allowed category
-                    print("illegal character")
-                    return False
-
-            #now have a count of all the lower, upper, special characters and numbers
-            if count_upper >= num_uppercase and count_lower >= num_lowercase and count_specials >= num_specials and count_numbers >= num_numbers:
-                return True
+        for letter in password:
+            #check each letter to see if special, lower, upper, or number. Count each of these
+            if letter.isdigit():
+                count_numbers += 1
+            elif letter.isupper():
+                count_upper += 1
+            elif letter.islower():
+                count_lower += 1
+            elif letter in allowed_specials:
+                count_specials += 1
             else:
+                #return false if part of password is not in any allowed category
+                print("illegal character")
                 return False
+
+        #now have a count of all the lower, upper, special characters and numbers
+        if count_upper >= num_uppercase and count_lower >= num_lowercase and count_specials >= num_specials and count_numbers >= num_numbers:
+            return True
+        else:
+            return False
 
     def check_user_details(self, password, user_firstname, user_lastname, user_birthyear):
-        #checks if the password contains the user name or year of birth. Outputs True if no user details in the password
+        #checks if the password contains the user name or year of birth. Outputs True if no user details in the password. Written by KW
         if user_firstname in password:
             return False
         elif user_lastname in password:
@@ -116,34 +156,55 @@ class PasswordChecker:
             return True
 
 
-    def generate_report(self,report):
-        #formats output in human-readable way. Checks if user wants this put into a file.
+    def generate_report(self,report, password):
+        #formats output in human-readable way. Checks if user wants this put into a file. Written by KW
+        print(f"Password was {password}")
         for line in report:
             print(line)
         #ask user if they want to write to file
         wants_file = input("Would you like this written to a file (y/n) ? ")
         if wants_file.lower() == "y":
-            write_to_file(report)
+            self.write_to_file(report, password)
         else:
             return "Report not written to file"
 
-    def write_to_file(self, report):
-        #writes report to file.
-        file = open(password_report.txt, 'w')
-        for line in report:
-            write.file(f"{line}\n")
-        return "Report written to password_report.txt"
+    def write_to_file(self, report, password):
+        while True:
+            #writes report to file. can append or overwrite. Written by KW
+            write_type = input("Please select 1 to append, or 2 to overwrite: ")
+
+            if write_type != "1" and write_type != "2":
+                print("Please input a valid option.")
+                continue
+
+            elif write_type == "1":
+                file = open('password_report.txt', 'a')
+
+            elif write_type == "2":
+                file = open('password_report.txt', 'w')
+
+
+            file.write(f"Password was {password} \n")
+            for line in report:
+                file.write(f"{line}\n")
+            return "Report written to password_report.txt"
+            break
+
+
+
 
     def read_password_policy(self):
-        #reads in password policy, returns variables as a list
+        #reads in password policy, returns variables as a list. Written by KW
         # password_policy = open('password_policy.txt', 'r')
-        num_specials = 1
-        num_lowercase = 3
-        num_uppercase = 3
-        num_numbers = 4
-        min_length = 8
-        max_length = 16
-        allowed_specials = "~!\"£$%^&*()_-+="
+        policy = configparser.ConfigParser()
+        policy.read('password_policy.txt')
+        num_specials = policy.getint('Policy', 'num_specials')
+        num_lowercase = policy.getint('Policy','num_lowercase')
+        num_uppercase = policy.getint('Policy','num_uppercase')
+        num_numbers = policy.getint('Policy','num_numbers')
+        min_length = policy.getint('Policy','min_length')
+        max_length = policy.getint('Policy','max_length')
+        allowed_specials = policy.get('Policy','allowed_specials')
 
         return [int(num_specials), int(num_lowercase), int(num_uppercase), int(num_numbers), int(min_length), int(max_length), allowed_specials]
 
@@ -151,8 +212,8 @@ class PasswordChecker:
 
     #########################TESTBED
 
-# password_tester = PasswordChecker()
+password_tester = PasswordChecker()
 # print("password_tester generated")
-# test_list = password_tester.check_password("Test_1929KIl","Kai", "Wolff", "1992")
-# print(test_list)
+test_list = password_tester.check_password('tunnel', 'Kai', 'Wolff', '1992')
+print(test_list)
 
---
+++ README.md
@@ -0,0 +1,3 @@
+common_passwords.txt - This list was taken from https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10k-most-common.txt
+
+It can be replaced with any list of passwords, as long as it is named "common_passwords.txt"
